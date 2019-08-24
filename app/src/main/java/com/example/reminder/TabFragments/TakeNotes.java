48555415632b60586506752d882a713eaa737baa
Kushal-Gera <kushalgera1212@gmail.com>
2019-08-24 17:13:57 +0530

    Working 'Take Notes'

--- app/src/main/java/com/example/reminder/TabFragments/TakeNotes.java
+++ app/src/main/java/com/example/reminder/TabFragments/TakeNotes.java
@@ -1,9 +1,12 @@
 package com.example.reminder.TabFragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,13 +15,26 @@ import androidx.fragment.app.Fragment;
 import com.example.reminder.R;
 
 public class TakeNotes extends Fragment {
+    private static final String TAG = "TakeNotes";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Button add_btn;
+        View view = inflater.inflate(R.layout.take_note, container, false);
 
-        View view = inflater.inflate(R.layout.add_reminder, container, false);
+        add_btn = view.findViewById(R.id.add_btn);
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "add btn clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         return view;
+
+
     }
+
 
 }
